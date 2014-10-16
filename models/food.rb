852ae751c4e36d26f3e32847a4f2e03e80cc2534
maninigupta <manini.gupta@gmail.com>
2014-10-15 20:04:00 -0400

    validate food uniqueness and presence, create error message

--- models/food.rb
+++ models/food.rb
@@ -1,4 +1,8 @@
 class Food < ActiveRecord::Base
+  validates :name, uniqueness: true
+  validates :name, :course, :price, presence: true
+
   has_many(:orders)
   has_many(:parties, :through => :orders)
 end 
+
