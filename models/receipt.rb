3331c7b75c893cca24a7f028667c54768168bcf9
maninigupta <manini.gupta@gmail.com>
2014-10-29 18:03:02 -0400

    fix receipt tip format, reformat party edit & delete buttons

--- models/receipt.rb
+++ models/receipt.rb
@@ -24,24 +24,27 @@ class Receipt
   end
 
   def subtotal
-    "Subtotal".ljust(20, '.') + 
+    "Subtotal".ljust(20, '.') +
     formatted_price("#{sum}").rjust(10, '.')
   end
 
   def formatted_price(price)
-    dollars, cents = price.split('.')
+    dollars, cents = price.to_f.to_s.split('.')
     cents = cents.ljust(2, "0")
     [dollars, cents].join(".")
   end
 
   def sum
-    prices = @foods.map {|food| 
+    prices = @foods.map {|food|
       food.price}
     prices.inject(:+)
   end
 
   def suggested_tip
-    "Suggested Tip:\n10% = #{sum*1.1}\n15% = #{sum*1.15}\n20% = #{sum*1.2}"
+    tip_suggestions = "Suggested Tip:\n"
+    tip_suggestions << "Total + 10% = #{formatted_price(sum*1.1)}\n"
+    tip_suggestions << "Total + 15% = #{formatted_price(sum*1.15)}\n"
+    tip_suggestions << "Total + 20% = #{formatted_price(sum*1.2)}"
     # calculate
   end
 
